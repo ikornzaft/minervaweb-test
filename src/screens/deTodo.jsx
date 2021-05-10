aa693b62b44d33119b4145aee5bb81a9eb24fab9
Nacho <ikornzaft@gmail.com>
2021-05-10 19:30:23 -0300

    build: add popover elements in deTodo screen

--- src/screens/deTodo.jsx
+++ src/screens/deTodo.jsx
@@ -4,15 +4,25 @@ import {
   Stack,
   Heading,
   Button,
-  Flex,
   Menu,
   MenuButton,
   MenuList,
   MenuItem,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  Box,
+  ButtonGroup
 } from '@chakra-ui/react';
 import { TiArrowSortedDown } from 'react-icons/ti';
 
 const DeTodo = () => {
+  const initialFocusRef = React.useRef();
   return (
     <Container
       maxWidth="container.lg"
@@ -20,14 +30,14 @@ const DeTodo = () => {
       backgroundColor="gray.50"
       padding="0px"
     >
-    <Stack direction="column" textAlign="center">
-    <Stack
-    backgroundColor="gray.50"
-    alignItems="center"
+      <Stack direction="column" textAlign="center">
+        <Stack
+          backgroundColor="gray.50"
+          alignItems="center"
           padding={2}
           paddingBottom={8}
           spacing={6}
-          >
+        >
           <Heading>De todo un poco</Heading>
           <Menu>
             <MenuButton as={Button} rightIcon={<TiArrowSortedDown />}>
@@ -41,10 +51,56 @@ const DeTodo = () => {
               <MenuItem>Attend a Workshop</MenuItem>
             </MenuList>
           </Menu>
-          </Stack>    
-          </Stack>
-          </Container>
-          );
-        };
+          <Popover>
+            <PopoverTrigger>
+              <Button>Popver</Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader>Pop Over!</PopoverHeader>
+              <PopoverBody>Esto es un popover!</PopoverBody>
+            </PopoverContent>
+          </Popover>
+          <Popover
+            initialFocusRef={initialFocusRef}
+            placement="bottom"
+            closeOnBlur={false}
+          >
+            <PopoverTrigger>
+              <Button>Otro popover</Button>
+            </PopoverTrigger>
+            <PopoverContent color="white" bg="blue.800" borderColor="blue.800">
+              <PopoverHeader pt={4} fontWeight="bold" border="0">
+                Este es otro popover
+              </PopoverHeader>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverBody>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore.
+              </PopoverBody>
+              <PopoverFooter
+                border="0"
+                d="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                pb={4}
+              >
+                <Box fontSize="sm">Paso 2 de 4</Box>
+                <ButtonGroup size="sm">
+                  <Button colorScheme="green">Configurar Email</Button>
+                  <Button colorScheme="blue" ref={initialFocusRef}>
+                    Sig.
+                  </Button>
+                </ButtonGroup>
+              </PopoverFooter>
+            </PopoverContent>
+          </Popover>
+        </Stack>
+      </Stack>
+    </Container>
+  );
+};
 
 export { DeTodo };
