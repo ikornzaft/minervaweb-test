546a599cce19212c32f0868adc63cefc3c56971c
nacho <ikornzaft@gmail.com>
2021-08-08 23:04:58 -0300

    refactor: minor changes in createQuiz

--- src/components/createQuiz/areaSelector.jsx
+++ src/components/createQuiz/areaSelector.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { HStack, Select, Text } from '@chakra-ui/react';
+
 import { AREAS } from '../../locals/sp/areas';
 
-const AreaSelector = ({newQuizWorkarea, setNewQuizWorkarea, workareaError}) => {
+const AreaSelector = ({ newQuizWorkarea, setNewQuizWorkarea, workareaError }) => {
   const options = [
     { key: AREAS.area_1.tag, value: AREAS.area_1.route },
     { key: AREAS.area_2.tag, value: AREAS.area_2.route },
@@ -11,19 +12,27 @@ const AreaSelector = ({newQuizWorkarea, setNewQuizWorkarea, workareaError}) => {
   ];
 
   return (
-    <HStack className="form-control" w="15rem" paddingY={2}>
-
-        <Select borderRadius="md" borderColor={workareaError ? "red" : null} size="sm" placeholder="Elegir la materia" onChange={e => setNewQuizWorkarea(e.target.value)}>
-          {options.map((option) => {
-            return (
-              <option key={option.value} value={option.value}>
-                {option.key}
-              </option>
-            );
-          })}
-        </Select>
-        {workareaError ? <Text fontSize="xs" right="100px" color="red" position="absolute">Es necesario seleccionar una materia</Text> : null}
-
+    <HStack className="form-control" paddingY={2} w="15rem">
+      <Select
+        borderColor={workareaError ? 'red' : null}
+        borderRadius="md"
+        placeholder="Elegir la materia"
+        size="sm"
+        onChange={(e) => setNewQuizWorkarea(e.target.value)}
+      >
+        {options.map((option) => {
+          return (
+            <option key={option.value} value={option.value}>
+              {option.key}
+            </option>
+          );
+        })}
+      </Select>
+      {workareaError ? (
+        <Text color="red" fontSize="xs" position="absolute" right="100px">
+          Es necesario seleccionar una materia
+        </Text>
+      ) : null}
     </HStack>
   );
 };
--- src/components/createQuiz/quizHeaderForm.jsx
+++ src/components/createQuiz/quizHeaderForm.jsx
@@ -4,7 +4,9 @@ import { Input, FormControl, FormLabel } from '@chakra-ui/react';
 const QuizHeaderForm = ({ setNewQuizTitle }) => {
   return (
     <FormControl>
-      <FormLabel paddingLeft={5} fontSize="14px">Título de la autoevaluación</FormLabel>
+      <FormLabel fontSize="14px" paddingLeft={5}>
+        Título de la autoevaluación
+      </FormLabel>
       <Input type="text" width="36rem" onChange={(e) => setNewQuizTitle(e.target.value)} />
     </FormControl>
   );
